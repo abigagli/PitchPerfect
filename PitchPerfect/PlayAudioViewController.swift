6eea5f03ccce5bbd2bc38159c39fd9f1ac8d295f
Andrea Bigagli <abigagli@gmail.com>
2015-03-15 22:43:11 +0100

    Completed lesson 4B

--- PitchPerfect/PlayAudioViewController.swift
+++ PitchPerfect/PlayAudioViewController.swift
@@ -13,6 +13,15 @@ class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
     
     //MARK: Stored properties
     private var audioPlayer : AVAudioPlayer!
+    private var audioEngine : AVAudioEngine!
+    private var audioFile : AVAudioFile!
+    
+    //Needed to work-around what seems to be a "quirk" (or a bug) in the way AVAudioPlayerNode's
+    //completion handler behaves: it seems to fire at least 2-3 seconds after the sound really stopped
+    //playing, so it's not really good as a mean to automatically hide the stop button.
+    //Resorting to an NSTimer-based solution instead, as read on SO
+    private var hideStopTimer : NSTimer!
+    
     var receivedAudio : RecordedAudio!
     
     //MARK: Computed properties
@@ -28,7 +37,16 @@ class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
     }
     private func updateState ()
     {
-        stopButton.hidden = (state == .stopped)
+        //The logic is simple: stopButton.hidden = (state == .stopped)
+        //but since I want to use animations, it's clearer to use an explicit switch 
+        //that calls 2 separate functions
+        
+        switch state{
+        case .ongoing:
+            showStopButton()
+        case .stopped:
+            hideStopButton()
+        }
     }
 
 
@@ -46,13 +64,17 @@ class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
     
     @IBAction func stopPlaying(sender: UIButton) {
         audioPlayer.stop()
+        audioEngine.stop()
+        hideStopTimer?.invalidate()
         state = .stopped
     }
     
     @IBAction func playChipmunkAudio(sender: UIButton) {
+        playSoundWithPitch(1000)
     }
     
     @IBAction func playDarthvaderAudio(sender: UIButton) {
+        playSoundWithPitch(-1000)
     }
     
     //MARK: Overrides
@@ -62,6 +84,9 @@ class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathURL, error: nil)
         audioPlayer.delegate = self
         audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathURL, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -81,6 +106,7 @@ class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
     
     //MARK: Business Logic
     private func playSoundWithRate(rate : Float, fromBeginning : Bool = true) {
+        hideStopTimer?.invalidate()
         audioPlayer.stop()
         if (fromBeginning) {
             audioPlayer.currentTime = 0
@@ -90,6 +116,46 @@ class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
         state = .ongoing
     }
     
+    private func playSoundWithPitch(pitch : Float)
+    {
+        hideStopTimer?.invalidate()
+        audioPlayer.stop()
+        
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+            
+        audioEngine.startAndReturnError(nil)
+        audioPlayerNode.play()
+        state = .ongoing
+        
+        //This is kind of a kludge, but read the comments about hideStopTimer for a rationale
+        if (audioPlayerNode.playing)
+        {
+            hideStopTimer?.invalidate()
+            hideStopTimer = NSTimer.scheduledTimerWithTimeInterval(audioPlayer.duration, target: self, selector: "hideStopButton", userInfo: nil, repeats: false)
+        }
+    }
+    
+    private func showStopButton()
+    {
+        stopButton.hidden = false
+    }
+    
+    func hideStopButton() //Can't make this private as that prevents it being usable as a selector in NSTimer's scheduledTimerWithTimeInterval
+    {
+        stopButton.hidden = true
+    }
     // MARK: - Navigation
 /*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
